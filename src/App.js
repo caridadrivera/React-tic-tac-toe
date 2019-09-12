df7ac09b1772d5a7a4af403be0dbcdc5c3419764
Caridad Rivera <ccrive10@gmail.com>
2019-09-11 22:04:56 -0400

    set up grid

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import './App.css';
 
 function App() {
   return (
+    /* this is a container of the whole page, this holds up the app*/
     <div className="container">
       <h1>Tic Tac Toe </h1>
+        <div className="board">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+        </div>
     </div>
   );
 }

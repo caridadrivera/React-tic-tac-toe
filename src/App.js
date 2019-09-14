08d8c11bdb8ff703c76f28f558ed819094022ec4
Caridad Rivera <ccrive10@gmail.com>
2019-09-14 16:22:25 -0400

    finishing up

--- src/App.js
+++ src/App.js
@@ -4,17 +4,41 @@ import './App.css';
 class App extends Component {
 
   state = {
-    board: Array(9).fill("X")
+    board: Array(9).fill(null)
   }
 
+  handleClick(index){
+    // console.log(e.target.id, "hiii")
+    // console.log(this.state.board,index);
+    let newBoard = this.state.board
+
+    newBoard[index] ="X"
+
+     console.log(index)
+     this.setState({
+       board: newBoard
+     });
+  };
 
   render() {
 
-    const Box = this.state.board.map(box => <div className="box">{box}</div>)
+    /* with the 9 boxes doing the same thing,
+     we need to distingish the boxes by a key id.
+     with index of the box in the array. */
+    const Box = this.state.board.map(
+
+      (box, index) =>
+
+      <div className="box"
+
+      key={index}
+
+      onClick={() => {this.handleClick(index)}}>{box}
+
+      </div>
+    )
 
     return (
-
-
       /* this is a container of the whole page, this holds up the app*/
       <div className="container">
         <h1>Tic Tac Toe </h1>
